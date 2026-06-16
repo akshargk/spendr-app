70ba76fd6b395cc52b2ce91f6b3f6acbe474954c
akshargk <akshar.gk.s.113@kalvium.community>
2026-06-16 14:50:45 +0530

    feat: day 5 navigation and expense tracking

--- App.js
+++ App.js
@@ -1,5 +1,50 @@
-import ProfileScreen from "./screens/ProfileScreen";
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import BottomTabs from './navigation/BottomTabs';
+
+import { FAKE_EXPENSES } from './constants/expenses';
 
 export default function App() {
-  return <ProfileScreen />;
+  const [showWelcome, setShowWelcome] =
+    useState(true);
+
+  const [expenses, setExpenses] =
+    useState(FAKE_EXPENSES);
+
+  const addExpense = (newExpense) => {
+    setExpenses((prev) => [
+      newExpense,
+      ...prev,
+    ]);
+  };
+
+  if (showWelcome) {
+    return (
+      <>
+        <StatusBar style="light" />
+
+        <WelcomeScreen
+          onGetStarted={() =>
+            setShowWelcome(false)
+          }
+        />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <StatusBar style="light" />
+
+      <NavigationContainer>
+        <BottomTabs
+          expenses={expenses}
+          addExpense={addExpense}
+        />
+      </NavigationContainer>
+    </>
+  );
 }
